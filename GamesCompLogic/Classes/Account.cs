2266cb78cc80be4679272b698433060769eb0592
Bjongen <488751@student.fontys.nl>
2022-05-13 16:12:25 +0200

    new rep

---
+++ GamesCompLogic/Classes/Account.cs
@@ -0,0 +1,47 @@
+﻿using GamesCompInterface.Dtos;
+using System;
+
+namespace GamesCompLogic
+{
+    public class Account
+    {
+        public int AccountId { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public Account(string name, string password, string email, string phoneNumber, bool isAdmin)
+        {
+            Name = name;
+            Password = password;
+            Email = email;
+            IsAdmin = isAdmin;
+            PhoneNumber = phoneNumber;
+        }
+
+        public Account(AccountDto accountDto)
+        {
+            AccountId = accountDto.AccountId;
+            Name = accountDto.Name;
+            Password = accountDto.Password;
+            Email = accountDto.Email;
+            PhoneNumber = accountDto.PhoneNumber;
+            IsAdmin = accountDto.IsAdmin;
+        }
+
+        public AccountDto ToDto()
+        {
+            return new AccountDto
+            {
+                AccountId = AccountId,
+                Name = Name,
+                Password = Password,
+                Email = Email,
+                PhoneNumber = PhoneNumber,
+                IsAdmin = IsAdmin
+            };
+        }
+    }
+}
---
+++ GamesCompLogic/Containers/AccountContainer.cs
@@ -0,0 +1,84 @@
+﻿using GamesCompDAL.IDAL;
+using GamesCompInterface.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesCompLogic.Containers
+{
+    public class AccountContainer
+    {
+        public List<Account> _accounts;
+
+        private IAccountDAL iAccountDAL;
+
+        public AccountContainer(IAccountDAL iAccountDAL)
+        {
+            this.iAccountDAL = iAccountDAL;
+            _accounts = new List<Account>();
+        }
+
+        public IReadOnlyCollection<Account> GetAccounts()
+        {
+            return _accounts;
+        }
+
+
+        public int DeleteAccount(Account account)
+        {
+            AccountDto accountDto = new AccountDto()
+            {
+                Name = account.Name,
+                Password = account.Password,
+                Email = account.Email,
+                PhoneNumber = account.PhoneNumber,
+                IsAdmin = account.IsAdmin
+            };
+
+            return iAccountDAL.DeleteAccount(accountDto);
+        }
+
+        public int CreateAccount(Account account)
+        {
+            AccountDto accountDto = new AccountDto()
+            {
+                Name = account.Name,
+                Password = account.Password,
+                Email = account.Email,
+                PhoneNumber = account.PhoneNumber,
+                IsAdmin = account.IsAdmin
+            };
+            _accounts.Add(new Account(accountDto));
+            return iAccountDAL.AddAccount(accountDto);
+        }
+
+        public bool CheckUserName(Account account)
+        {
+            List<AccountDto> accountDtos = new();
+            accountDtos = iAccountDAL.GetALL().ToList();
+            foreach(var accountdto in accountDtos)
+            {
+                if(accountdto.Name == account.Name)
+                {
+                    return true;
+                }
+            }
+            return false;
+            
+        }
+
+        public IList<Account> GetAllAccounts()
+        {
+            var accountDtos = iAccountDAL.GetALL();
+            List<Account> accounts = new();
+            foreach(var dto in accountDtos)
+            {
+                accounts.Add(new Account(dto));
+            }
+
+            return _accounts = accounts;
+        }
+    }
+}
